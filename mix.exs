5a386826eb8483ad4b5cb013a0be185f59c4c8ab
Derek Kraan <derek@kraan.dev>
2024-06-04 08:15:47 +0200

    Bump version to 0.98.1

--- mix.exs
+++ mix.exs
@@ -11,7 +11,7 @@ defmodule CurlReq.MixProject do
       package: package(),
       source_url: "https://github.com/derekkraan/curl_req",
       start_permanent: Mix.env() == :prod,
-      version: "0.98.0"
+      version: "0.98.1"
     ]
   end
 
